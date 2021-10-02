b10d3ddabffc677c913d4624b50d9816c377bcad
marshmallow <40532058+mrshmllow@users.noreply.github.com>
2021-10-02 12:00:22 +1000

    revert Correctly remove recipe book button

--- src/main/java/net/marshmallow/BetterRecipeBook/Mixins/RemoveBookButton.java
+++ src/main/java/net/marshmallow/BetterRecipeBook/Mixins/RemoveBookButton.java
@@ -1,18 +1,25 @@
 package net.marshmallow.BetterRecipeBook.Mixins;
 
 import net.marshmallow.BetterRecipeBook.BetterRecipeBook;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(InventoryScreen.class)
+@Mixin(TexturedButtonWidget.class)
 public class RemoveBookButton {
-    // The targeting is not particularly elegant
-    @Inject(method = "init", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/gui/screen/recipebook/RecipeBookWidget;findLeftEdge(II)I"), cancellable = true)
-    private void mixin(CallbackInfo ci) {
-        if (!BetterRecipeBook.config.enableBook) {
+    @Final
+    @Shadow
+    private Identifier texture;
+
+    @Inject(at = @At("HEAD"), method = "renderButton(Lnet/minecraft/client/util/math/MatrixStack;IIF)V", cancellable = true)
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if (texture.equals(new Identifier("minecraft:textures/gui/recipe_button.png")) && !BetterRecipeBook.config.enableBook) {
             ci.cancel();
         }
     }
