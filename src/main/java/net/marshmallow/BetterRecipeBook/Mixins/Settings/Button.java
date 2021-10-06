c59fc284d5187264ec218def3ee756ac4014d062
marshmallow <40532058+mrshmllow@users.noreply.github.com>
2021-10-06 12:29:46 +1100

    added settings button

---
+++ src/main/java/net/marshmallow/BetterRecipeBook/Mixins/Settings/Button.java
@@ -0,0 +1,90 @@
+package net.marshmallow.BetterRecipeBook.Mixins.Settings;
+
+import me.shedaniel.autoconfig.AutoConfig;
+import net.marshmallow.BetterRecipeBook.BetterRecipeBook;
+import net.marshmallow.BetterRecipeBook.Config.Config;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RecipeBookWidget.class)
+public abstract class Button {
+    private static final Identifier BUTTON_TEXTURE = new Identifier("betterrecipebook:textures/gui/buttons.png");
+    @Shadow
+    protected MinecraftClient client;
+
+    @Shadow private int parentHeight;
+    @Shadow private int parentWidth;
+    @Shadow private int leftOffset;
+
+    @Shadow public abstract boolean isOpen();
+
+    protected TexturedButtonWidget settingsButton;
+    private static final Text OPEN_SETTINGS_TEXT;
+
+    @Inject(method = "reset", at = @At("RETURN"))
+    public void reset(CallbackInfo ci) {
+        if (!BetterRecipeBook.config.settingsButton) {
+            return;
+        }
+
+        int i = (this.parentWidth - 147) / 2 - this.leftOffset;
+        int j = (this.parentHeight - 166) / 2;
+
+        int u = 0;
+        if (BetterRecipeBook.config.darkMode) {
+            u = 18;
+        }
+
+        this.settingsButton = new TexturedButtonWidget(i + 11, j + 137, 16, 16, u, 73, 18, BUTTON_TEXTURE, button -> {
+            System.out.println("Yes");
+            MinecraftClient.getInstance().setScreen(AutoConfig.getConfigScreen(Config.class, MinecraftClient.getInstance().currentScreen).get());
+        });
+    }
+
+    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
+    public void mouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        if (this.settingsButton.mouseClicked(mouseX, mouseY, button) && this.isOpen()) {
+            assert this.client.player != null;
+            if (!this.client.player.isSpectator()) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/recipebook/RecipeBookResults;draw(Lnet/minecraft/client/util/math/MatrixStack;IIIIF)V"))
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if (!BetterRecipeBook.config.settingsButton) {
+            return;
+        }
+
+        this.settingsButton.render(matrices, mouseX, mouseY, delta);
+    }
+
+    @Inject(method = "drawTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/recipebook/RecipeBookWidget;drawGhostSlotTooltip(Lnet/minecraft/client/util/math/MatrixStack;IIII)V"))
+    public void drawTooltip(MatrixStack matrices, int x, int y, int mouseX, int mouseY, CallbackInfo ci) {
+        if (!BetterRecipeBook.config.settingsButton) {
+            return;
+        }
+
+        if (this.settingsButton.isHovered()) {
+            if (this.client.currentScreen != null) {
+                this.client.currentScreen.renderTooltip(matrices, OPEN_SETTINGS_TEXT, mouseX, mouseY);
+            }
+        }
+    }
+
+    static {
+        OPEN_SETTINGS_TEXT = new TranslatableText("betterrecipebook.gui.settings.open");
+    }
+}
