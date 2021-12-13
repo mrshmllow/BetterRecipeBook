45604513e935a22aef8390e0cc87504376b5f49b
marshmallow <marshycity@gmail.com>
2021-12-13 11:19:31 +1100

    fix searching issues

--- src/main/java/net/marshmallow/BetterRecipeBook/Mixins/UnGroup/Search.java
+++ src/main/java/net/marshmallow/BetterRecipeBook/Mixins/UnGroup/Search.java
@@ -1,16 +1,18 @@
 package net.marshmallow.BetterRecipeBook.Mixins.UnGroup;
 
-import it.unimi.dsi.fastutil.objects.ObjectSet;
 import net.marshmallow.BetterRecipeBook.BetterRecipeBook;
+import net.minecraft.client.ClientRecipeBook;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookPage;
 import net.minecraft.client.gui.screens.recipebook.RecipeCollection;
+import net.minecraft.world.inventory.RecipeBookMenu;
 import net.minecraft.world.item.crafting.Recipe;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
@@ -19,9 +21,12 @@ import java.util.Locale;
 @Mixin(RecipeBookComponent.class)
 public class Search {
     @Shadow private String lastSearch;
+    @Shadow private ClientRecipeBook book;
+    @Shadow protected RecipeBookMenu<?> menu;
+    @Shadow @Final private RecipeBookPage recipeBookPage;
 
-    @Inject(method = "updateCollections", locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;<init>(Ljava/util/Collection;)V"))
-    private void refreshSearchResults(boolean arg0, CallbackInfo ci, List<RecipeCollection> list, List<RecipeCollection> list2, String string) {
+    @Inject(method = "updateCollections", locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectLinkedOpenHashSet;<init>(Ljava/util/Collection;)V"), cancellable = true)
+    private void refreshSearchResults(boolean bl, CallbackInfo ci, List<RecipeCollection> list, List<RecipeCollection> list2, String string) {
         if (BetterRecipeBook.config.alternativeRecipes.noGrouped) {
             list2.removeIf((recipeResultCollection) -> {
                 for (Recipe<?> recipe : recipeResultCollection.getRecipes()) {
@@ -29,13 +34,13 @@ public class Search {
                 }
                 return false;
             });
+
+            if (this.book.isFiltering(this.menu)) {
+                list2.removeIf((recipeCollection) -> !recipeCollection.hasCraftable());
+            }
+
+            this.recipeBookPage.updateCollections(list2, bl);
+            ci.cancel();
         }
     }
-
-    //@Inject(method = "method_2594(Lit/unimi/dsi/fastutil/objects/ObjectSet;Lnet/minecraft/client/gui/screen/recipebook/RecipeResultCollection;)Z", at = @At("RETURN"), cancellable = true)
-    //private static void cancelContains(ObjectSet resultCollection, RecipeCollection recipeResultCollection, CallbackInfoReturnable<Boolean> cir) {
-    //    if (BetterRecipeBook.config.alternativeRecipes.noGrouped) {
-    //        cir.setReturnValue(false);
-    //    }
-    //}
 }
