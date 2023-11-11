f2b9efc9bbbab1f69c34fa0709c897bcb9feec0c
marshmallow <marshycity@gmail.com>
2023-11-12 10:35:03 +1100

    remove BBRBookSettings TypeSettings

--- common/src/main/java/marsh/town/brb/api/BBRBookSettings.java
+++ common/src/main/java/marsh/town/brb/api/BBRBookSettings.java
@@ -1,14 +1,16 @@
 package marsh.town.brb.api;
 
 import marsh.town.brb.BetterRecipeBook;
+import marsh.town.brb.mixins.accessors.BookSettingsTypeSettingsAccessor;
 import marsh.town.brb.util.BRBHelper;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.RecipeBookSettings;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class BBRBookSettings {
-    public static Map<ResourceLocation, TypeSettings> states = new HashMap<>();
+    public static Map<ResourceLocation, RecipeBookSettings.TypeSettings> states = new HashMap<>();
 
     public BBRBookSettings() {
         states = new HashMap<>();
@@ -16,58 +18,28 @@ public class BBRBookSettings {
 
     public static void registerBook(BRBHelper.Book book) {
         BetterRecipeBook.LOGGER.info("Registering book {}", book.resourceLocation);
-        states.put(book.resourceLocation, new TypeSettings(false, false));
+        states.put(book.resourceLocation, new RecipeBookSettings.TypeSettings(false, false));
     }
 
     public static boolean isOpen(BRBHelper.Book book) {
-        return states.get(book.resourceLocation).open;
+        RecipeBookSettings.TypeSettings settings = states.get(book.resourceLocation);
+
+        return ((BookSettingsTypeSettingsAccessor) settings).isOpen();
     }
 
     public static void setOpen(BRBHelper.Book book, boolean bl) {
-        states.get(book.resourceLocation).open = bl;
+        ((BookSettingsTypeSettingsAccessor) states.get(book.resourceLocation)).setOpen(bl);
     }
 
     public static boolean isFiltering(BRBHelper.Book book) {
-        return states.get(book.resourceLocation).filtering;
+        return ((BookSettingsTypeSettingsAccessor) states.get(book.resourceLocation)).isFiltering();
     }
 
     public static void setFiltering(BRBHelper.Book book, boolean bl) {
-        states.get(book.resourceLocation).filtering = bl;
+        ((BookSettingsTypeSettingsAccessor) states.get(book.resourceLocation)).setFiltering(bl);
     }
 
     public int hashCode() {
         return states.hashCode();
     }
-
-    static final class TypeSettings {
-        boolean open;
-        boolean filtering;
-
-        public TypeSettings(boolean bl, boolean bl2) {
-            this.open = bl;
-            this.filtering = bl2;
-        }
-
-        public boolean equals(Object object) {
-            if (this == object) {
-                return true;
-            }
-            if (object instanceof TypeSettings) {
-                TypeSettings typeSettings = (TypeSettings) object;
-                return this.open == typeSettings.open && this.filtering == typeSettings.filtering;
-            }
-            return false;
-        }
-
-        public int hashCode() {
-            int i = this.open ? 1 : 0;
-            i = 31 * i + (this.filtering ? 1 : 0);
-            return i;
-        }
-
-        public String toString() {
-            return "[open=" + this.open + ", filtering=" + this.filtering + "]";
-        }
-    }
-
 }
