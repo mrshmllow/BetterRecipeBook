c4d2d2be43ed9635166f0583017a64f1cad100cb
marshmallow <marshycity@gmail.com>
2024-01-22 13:26:03 +1100

    dev: add flake.nix direnv
    :<
    
    (cherry picked from commit 78b3e75581e6bf21012c8a62fca1c8af09d16945)

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "BRB Dev Shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          libglvnd
+          openjdk17
+        ];
+      };
+    });
+}
